5646de8e49084f093ea7409c37f7bf0dba62b0b3
Caroline Rives <car.rives@gmail.com>
2022-07-25 15:06:33 -0400

    Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+    /*
+    The starter code is supposed to find the sum of two inclusive integer ranges
+    in parallel but it’s not getting the correct result. Fix the code so that it
+    gives the correct sum.
+
+Example
+// Input
+2 6
+1 4
+
+// Output
+30
+     */
     private static final long mainThreadId = Thread.currentThread().getId();
 
     public static void main(String[] args) throws InterruptedException {
@@ -14,11 +27,22 @@ public class Main {
 
         RangeAdder adder1 = new RangeAdder(start1, end1);
         RangeAdder adder2 = new RangeAdder(start2, end2);
+        System.out.println("hello from the thread!");
 
-        long partialSum1 = adder1.getSum();
-        long partialSum2 = adder2.getSum();
+        // First burger
+        adder1.start(); // Start burger
+        adder1.join(); // Wait until burger is done
+        long partialSum1 = adder1.getSum(); // Get burger
+        System.out.println("paritalSum1 = " + partialSum1);
 
-        long sum = partialSum1 + partialSum2; 
+        // Second burger
+        adder2.start(); // Start second burger
+        adder2.join(); // Wait until second burger is done
+        long partialSum2 = adder2.getSum(); // Get second burger
+        System.out.println("paritalSum2 = " + partialSum2);
+
+        long sum = partialSum1 + partialSum2;
+        System.out.println("Hello from the after sum!");
 
         System.out.println(sum);
 
